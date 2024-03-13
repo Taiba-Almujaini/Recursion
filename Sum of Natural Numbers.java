c4c3ddce4e0cbaa6731d6cc9f72c1ab4dac96ec7
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-03-13 10:32:28 +0400

    calculates the sum of the first N natural numbers using recursion.

---
+++ Sum of Natural Numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NaturalNumber {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter positive number");// ask user to enter positive number
+        Integer positiveNumber = scanner.nextInt();
+        Integer sumOfNaturalNumber;
+        if (positiveNumber < 0) { // check if number is positive
+            System.out.println("Please Enter Positive number");
+        } else {
+            sumOfNaturalNumber=sumOfNumbers(positiveNumber); // call sumOfNumbers function
+            System.out.println("Numbers of digit in " + positiveNumber + " is: " + sumOfNaturalNumber);
+        }
+    }
+    //count the digit
+    public static Integer sumOfNumbers( int number) {
+
+        if (number == 0) {
+            return 0;// if numer 0 mean it has 0 digit
+        }
+        return  number + sumOfNumbers(number-1);// Add the current number to the sum of the previous natural numbers
+    }
+
+}
